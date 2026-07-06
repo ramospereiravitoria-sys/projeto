851808e00ebd14ef462ef62dd638408497c03294
ramospereiravitoria-sys <ramos.pereira.vitoria@escola.pr.gov.br>
2026-07-06 12:27:19 -0300

    f

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 const botoes = document.querySelectorAll("button")
 console.log(botoes)
 
+botoes.forEach(function (botao){
+    let curtiu = false
+    botao.addEventListener("click", botaoClicado)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function botaoClicado(){
+        let texto = document.querySelector("span")
+        if (curtiu = false){
+            texto.textContent++
+        }else{
+            texto.textContent--
+            curtiu = false
+        }
+    }
+})
 
 
 
